f8c4c1935b36de5bd0f5e60dc8efce1563dc3074
Elijah H. Ellis <smurftown2@gmail.com>
2016-05-05 08:15:42 -0400

    Updated reds

---
+++ test.js
@@ -0,0 +1,23 @@
+var odds = evens.map(v => v + 1);
+
+class Point extends FOO.bar {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+  }
+
+  get x(){
+    return this.x;
+  }
+
+  get y(){
+    return this.y;
+  }
+}
+
+document.querySelector('#id');
+
+let x = document.querySelector('#id');
+
+
+x.addClass(className);
